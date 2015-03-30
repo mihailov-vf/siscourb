d139070b5f07cc18daac59eb871412771d764916
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2015-03-30 20:34:53 -0300

    Factories para o Form de Usuário
    Realiza a ligação da Entidade, Form e Doctrine

--- module/User/config/service.config.php
+++ module/User/config/service.config.php
@@ -6,6 +6,12 @@ return array(
             'Siscourb\User\Mapper\UserMapper' => 'Siscourb\User\Mapper\UserMapperFactory',
         ),
     ),
+    'form_elements'=>array(
+        'factories' => array(
+            'Siscourb\User\Form\UserFieldset' => 'Siscourb\User\Form\UserFieldsetFactory',
+            'Siscourb\User\Form\UserForm' => 'Siscourb\User\Form\UserFormFactory',
+        ),
+    ),
     'controllers' => array(
         'factories' => array(
             'Siscourb\User\Controller\User' => 'Siscourb\User\Controller\UserControllerFactory',
