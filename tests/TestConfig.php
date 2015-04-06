a17c94dc3715b31362796a1f900335310d1c0925
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2015-04-06 18:31:56 -0300

    Ajustes para travis e coveralls

--- tests/TestConfig.php
+++ tests/TestConfig.php
@@ -1,5 +1,4 @@
 <?php
 $config = require 'config/application.config.php';
-//$config['module_listener_options']['config_glob_paths'] = array('config/autoload/{,*.}{global,local,test}.php');
 
 return $config;
