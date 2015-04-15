a91584b70235a41ad5959a4ead4e0b1e2a169e0c
mihailov <mihailov@mihailov-k46ca>
2015-04-15 11:53:33 -0300

    Configurações e acertos para testes do módulo de Organização

--- config/application.config.php
+++ config/application.config.php
@@ -16,6 +16,7 @@ return array(
         
         'Siscourb\Application',
         'Siscourb\User',
+        'Siscourb\Organization',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
