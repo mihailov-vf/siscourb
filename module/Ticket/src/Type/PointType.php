5b6eb70ac30ced9101c953e2435b0eabbeedd643
mihailov <mihailov@mihailov-k46ca>
2015-04-16 10:29:00 -0300

    Modelagem inicial do Módulo de Chamados(Ticket)
    PointType

---
+++ module/Ticket/src/Type/PointType.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Copyright (C) 2015 Mihailov Vasilievic Filho
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Siscourb\Ticket\Type;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Siscourb\Ticket\ValueObject\Point;
+
+/**
+ * Description of PointType
+ *
+ * @author Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
+ */
+class PointType extends Type
+{
+
+    const POINT = 'point';
+
+    public function getName()
+    {
+        return self::POINT;
+    }
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'POINT';
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        list($latitude, $longitude) = sscanf($value, 'POINT(%f %f)');
+
+        return new Point($latitude, $longitude);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof Point) {
+            $value = sprintf('POINT(%F %F)', $value->getLongitude(), $value->getLatitude());
+        }
+
+        return $value;
+    }
+
+    public function canRequireSQLConversion()
+    {
+        return true;
+    }
+
+    public function convertToPHPValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('AsText(%s)', $sqlExpr);
+    }
+
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('PointFromText(%s)', $sqlExpr);
+    }
+}
