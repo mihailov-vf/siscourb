446162d83ff48880df9ab2c64d2d6f3dda2942ca
mihailov <mihailov@mihailov-k46ca>
2015-04-20 16:18:58 -0300

    Modelagem inicial do Módulo de Chamados(Ticket)
    Repositório da entidade Ticket

---
+++ module/Ticket/src/Repository/TicketRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Copyright (C) 2015 Mihailov Vasilievic Filho
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Siscourb\Ticket\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Siscourb\Ticket\Entity\Ticket;
+use Siscourb\Ticket\Mapper\TicketMapperInterface;
+
+/**
+ * Description of TicketRepository
+ *
+ * @author Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
+ */
+class TicketRepository extends EntityRepository implements TicketMapperInterface
+{
+
+    public function insert(Ticket $ticket)
+    {
+        $this->_em->persist($ticket);
+        $this->_em->flush($ticket);
+
+        return $ticket;
+    }
+
+    public function update(Ticket $ticket)
+    {
+        $this->_em->flush($ticket);
+
+        return $ticket;
+    }
+
+    public function delete(Ticket $ticket)
+    {
+        $this->_em->remove($ticket);
+        $this->_em->flush($ticket);
+    }
+}
